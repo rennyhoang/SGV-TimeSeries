71de7dc0ed05726431a6bf0b145afe60e9fa566d
Renny Hoang <renny@rennyhoang.com>
2024-07-07 22:57:41 -0500

    update average

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
-from flask import Flask, render_template, request
-from dateutil.parser import parse
 import csv
+
+import h5py
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import h5py
+from dateutil.parser import parse
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-ts_file = './data/sentinel_dvert_timeseries_median_filtered.h5'
-disp_file = './data/ifgram_recon_displacement.h5'
-mask_file = './data/mask_low_elevation_high_tempCoh.h5'
-well_file = './data/well_SanGabriel.txt'
+ts_file = "./data/sentinel_dvert_timeseries_median_filtered.h5"
+disp_file = "./data/ifgram_recon_displacement.h5"
+mask_file = "./data/mask_low_elevation_high_tempCoh.h5"
+well_file = "./data/well_SanGabriel.txt"
 plot_filename = None
 snapshot_filename = None
 coordinates = ""
 
 
 def averagetimeseries(dset, dates):
+    """
     # iterate over all the times
     res = []
     for time in dset:
@@ -31,14 +33,26 @@ def averagetimeseries(dset, dates):
         total /= n
         res.append(total)
     return np.array(res)
+    """
+    res = []
+    for time in dset:
+        total = 0
+        n = 0
+        for lat in time[205:216]:
+            for measurement in lat[205:216]:
+                total += measurement
+                n += 1
+        total /= n
+        res.append(total)
+    return np.array(res)
 
 
 def readtimeseries(filepath):
     # read the data
-    f = h5py.File(filepath, 'r')
-    dset = f['timeseries']
+    f = h5py.File(filepath, "r")
+    dset = f["timeseries"]
     dset = np.array(dset)
-    dates = [i.decode('utf8') for i in f['date'][:]]
+    dates = [i.decode("utf8") for i in f["date"][:]]
     dates = pd.to_datetime(dates)
     f.close()
     return dset, dates
@@ -46,8 +60,8 @@ def readtimeseries(filepath):
 
 def readdisplacement(filepath):
     # read the data
-    f = h5py.File(filepath, 'r')
-    dset = f['velocity']
+    f = h5py.File(filepath, "r")
+    dset = f["velocity"]
     dset = np.array(dset)
     f.close()
     return dset
@@ -55,8 +69,8 @@ def readdisplacement(filepath):
 
 def readmask(filepath):
     # read the data
-    f = h5py.File(filepath, 'r')
-    dset = f['mask']
+    f = h5py.File(filepath, "r")
+    dset = f["mask"]
     dset = np.array(dset)
     f.close()
     return dset
@@ -74,16 +88,16 @@ def latlon2yx(lat, lon):
     return y, x
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():  # put application's code here
     global plot_filename, snapshot_filename, coordinates
 
-    if request.method == 'POST':
-        if 'latitude' in request.form:
+    if request.method == "POST":
+        if "latitude" in request.form:
             # parse ts data
             dset_ts, dates = readtimeseries(ts_file)
-            lat = float(request.form['latitude'])
-            lon = float(request.form['longitude'])
+            lat = float(request.form["latitude"])
+            lon = float(request.form["longitude"])
             y, x = latlon2yx(lat, lon)
             coordinates = "Pixel location: " + str(y) + ", " + str(x)
             ts_yx = dset_ts[:, y, x]
@@ -93,7 +107,7 @@ def index():  # put application's code here
                 csv_reader = csv.reader(csvfile)
                 fields = next(csv_reader)
                 for row in csv_reader:
-                    date = parse(row[0]).strftime('%Y-%m-%d')
+                    date = parse(row[0]).strftime("%Y-%m-%d")
                     year = int(date[:4])
                     measurement = row[1]
                     if year >= 2015:
@@ -104,46 +118,50 @@ def index():  # put application's code here
 
             # plot well/ts data
             fig, ax1 = plt.subplots()
-            ax1.set_xlabel('Date')
-            ax1.set_ylabel('Displacement [m]')
-            ax1.scatter(dates, ts_yx, c='k')
+            ax1.set_xlabel("Date")
+            ax1.set_ylabel("Displacement [m]")
+            ax1.scatter(dates, ts_yx, c="k")
             ax2 = ax1.twinx()
-            ax2.set_ylabel('Measurement [mm]')
-            ax2.scatter(well_dates, measurements, c='c')
+            ax2.set_ylabel("Measurement [mm]")
+            ax2.scatter(well_dates, measurements, c="c")
             fig.tight_layout()
             plot_filename = "static/plot.png"
-            fig.savefig(plot_filename, bbox_inches='tight')
+            fig.savefig(plot_filename, bbox_inches="tight")
 
-            return render_template('index.html',
-                                   plot_filename=plot_filename,
-                                   snapshot_filename=snapshot_filename,
-                                   coordinates=coordinates)
+            return render_template(
+                "index.html",
+                plot_filename=plot_filename,
+                snapshot_filename=snapshot_filename,
+                coordinates=coordinates,
+            )
 
-        elif 'date1' in request.form:
+        elif "date1" in request.form:
             dset_ts, dates = readtimeseries(ts_file)
             print(dates)
             dset_mask = readmask(mask_file)
-            date1 = str(request.form['date1'])
-            date2 = str(request.form['date2'])
+            date1 = str(request.form["date1"])
+            date2 = str(request.form["date2"])
             idx1 = dates.get_loc(date1)
             idx2 = dates.get_loc(date2)
             disp_idx1_idx2 = dset_ts[idx2,] - dset_ts[idx1,]
             plt.figure()
-            plt.imshow(disp_idx1_idx2 * dset_mask, cmap='jet', vmin=-0.05, vmax=0.05)
+            plt.imshow(disp_idx1_idx2 * dset_mask,
+                       cmap="jet", vmin=-0.05, vmax=0.05)
             snapshot_filename = "static/snapshot.png"
-            plt.savefig(snapshot_filename, bbox_inches='tight')
+            plt.savefig(snapshot_filename, bbox_inches="tight")
 
-            return render_template('index.html',
-                                   plot_filename=plot_filename,
-                                   snapshot_filename=snapshot_filename,
-                                   coordinates=coordinates)
+            return render_template(
+                "index.html",
+                plot_filename=plot_filename,
+                snapshot_filename=snapshot_filename,
+                coordinates=coordinates,
+            )
 
     # Render the form
-    return render_template('index.html',
-                           plot_filename=None,
-                           snapshot_filename=None,
-                           coordinates='')
+    return render_template(
+        "index.html", plot_filename=None, snapshot_filename=None, coordinates=""
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
--- average.py
+++ average.py
@@ -1,25 +1,37 @@
-from dateutil.parser import parse
 import csv
+import sys
+
+import h5py
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import h5py
-import sys
+from dateutil.parser import parse
+from matplotlib.ticker import FormatStrFormatter, StrMethodFormatter
 
-ts_file = './data/sentinel_dvert_timeseries_median_filtered.h5'
-disp_file = './data/ifgram_recon_displacement.h5'
-mask_file = './data/mask_low_elevation_high_tempCoh.h5'
-well_file = './data/well_SanGabriel.txt'
+ts_file = "./data/sentinel_dvert_timeseries_median_filtered.h5"
+disp_file = "./data/ifgram_recon_displacement.h5"
+mask_file = "./data/mask_low_elevation_high_tempCoh.h5"
+well_file = "./data/well_SanGabriel.txt"
 
-np.set_printoptions(threshold=sys.maxsize)
+np.set_printoptions(threshold=np.inf)
 
 
-def calculate_storage_loss(dset):
-    # get storage loss
-    total_diff = np.nan_to_num(dset[-1,] - dset[0,])
-    total_change = np.trapezoid(np.trapezoid(total_diff, axis=1), axis=0)
-    print(total_diff)
-    print(total_change)
+def calculate_storage_loss(dset, idx1, idx2):
+    meters_to_acrefeet = 350656.168
+    diff = dset[idx2,] - dset[idx1,]
+
+    total = 0
+    n = 0
+    for lat in diff[115:126]:
+        for measurement in lat[260:271]:
+            if np.isnan(measurement):
+                continue
+            total += measurement
+            n += 1
+    avg = total / n
+    total_disp = avg * meters_to_acrefeet
+
+    return total_disp
 
 
 def averagetimeseries(dset, dates):
@@ -29,23 +41,26 @@ def averagetimeseries(dset, dates):
         # average displacement over all x, y
         total = 0
         n = 0
-        for lat in time:
-            for measurement in lat:
+        for lat in time[115:126]:
+            for measurement in lat[260:271]:
                 if np.isnan(measurement):
                     continue
                 total += measurement
                 n += 1
-        total /= n
-        res.append(total)
+        total /= n if n != 0 else 1
+        if total > -0.01:
+            res.append(total)
+        else:
+            res.append(np.nan)
     return np.array(res)
 
 
 def readtimeseries(filepath):
     # read the data
-    f = h5py.File(filepath, 'r')
-    dset = f['timeseries']
+    f = h5py.File(filepath, "r")
+    dset = f["timeseries"]
     dset = np.array(dset)
-    dates = [i.decode('utf8') for i in f['date'][:]]
+    dates = [i.decode("utf8") for i in f["date"][:]]
     dates = pd.to_datetime(dates)
     f.close()
     return dset, dates
@@ -53,8 +68,8 @@ def readtimeseries(filepath):
 
 def readmask(filepath):
     # read the data
-    f = h5py.File(filepath, 'r')
-    dset = f['mask']
+    f = h5py.File(filepath, "r")
+    dset = f["mask"]
     dset = np.array(dset)
     f.close()
     return dset
@@ -64,7 +79,7 @@ def main():
     dset_ts, dates = readtimeseries(ts_file)
     dset_mask = readmask(mask_file)
     dset_ts *= dset_mask
-    calculate_storage_loss(dset_ts)
+    print(calculate_storage_loss(dset_ts, 351, 381))
     averaged_dset = averagetimeseries(dset_ts, dates)
 
     # parse well data
@@ -73,7 +88,7 @@ def main():
         csv_reader = csv.reader(csvfile)
         fields = next(csv_reader)
         for row in csv_reader:
-            date = parse(row[0]).strftime('%Y-%m-%d')
+            date = parse(row[0]).strftime("%Y-%m-%d")
             year = int(date[:4])
             measurement = row[1]
             if year >= 2015:
@@ -82,17 +97,76 @@ def main():
             well_dates = pd.to_datetime(well_dates)
             measurements = np.array([float(row[1]) for row in rows])
 
+    plt.figure(figsize=(20, 5))
     fig, ax1 = plt.subplots()
-    ax1.set_xlabel('Date')
-    ax1.set_ylabel('Displacement [m]')
-    ax1.scatter(dates, averaged_dset, c='k')
+    fig.set_figwidth(10)
+    ax1.set_xlabel("Time [y]", labelpad=10)
+    ax1.set_ylabel("InSAR Displacement [m]", labelpad=0)
     ax2 = ax1.twinx()
-    ax2.set_ylabel('Measurement [mm]')
-    ax2.scatter(well_dates, measurements, c='c')
+    ax2.set_ylabel("Well Measurement [mm]", labelpad=10)
+
+    water_prod_dates = pd.to_datetime(
+        [
+            "2015-10-1",
+            "2016-10-1",
+            "2017-10-1",
+            "2018-10-1",
+            "2019-10-1",
+            "2020-10-1",
+            "2021-10-1",
+            "2022-10-1",
+        ]
+    )
+    water_prod_vals = [
+        182826.49,
+        197243.28,
+        209499.70,
+        190156.12,
+        192583.66,
+        207821.52,
+        186184.04,
+        168360.09,
+    ]
+
+    ax3 = ax1.twinx()
+    ax3.spines.right.set_position(("axes", 1.2))
+    ax3.set_ylabel("Water Production [acre/feet]", labelpad=10)
+    ax3.set_ylim(150000, 300000)
+    ax3.yaxis.set_major_formatter(StrMethodFormatter("{x:,}"))
+    ax1.set_zorder(ax2.get_zorder() + 1)
+    ax1.patch.set_visible(False)
+
+    ax1.scatter(dates, averaged_dset, c="k", s=4, label="InSAR", zorder=10)
+    line3 = ax3.bar(
+        water_prod_dates,
+        water_prod_vals,
+        width=300,
+        zorder=1000,
+        align="edge",
+        color="#ECE27D80",
+    )
+    ax2.plot(
+        well_dates,
+        measurements,
+        c="b",
+        label="Well Data",
+        linestyle="dashed",
+        zorder=11,
+    )
+
+    lines_1, labels_1 = ax1.get_legend_handles_labels()
+    lines_2, labels_2 = ax2.get_legend_handles_labels()
+
+    lines = lines_1 + lines_2
+    labels = labels_1 + labels_2
+
+    ax1.legend(lines + [line3], labels +
+               ["Water Production"], loc=0, frameon=False)
+
     fig.tight_layout()
-    plot_filename = "static/average-plot.png"
-    fig.savefig(plot_filename, bbox_inches='tight')
+    plot_filename = "static/new-average-plot.png"
+    fig.savefig(plot_filename, bbox_inches="tight")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
